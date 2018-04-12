d2d93babefa61163fef64d4a6fc64e89109fa6c5
CallumHemsley <bythepenguin@gmail.com>
2018-04-12 10:57:26 +0100

    cleaned up posts a little

--- frontend/app/src/components/post/Posts.js
+++ frontend/app/src/components/post/Posts.js
@@ -16,18 +16,42 @@ class Posts extends React.Component{
       const { isAuthenticated } = auth;
       //return JSX, map method creates array with results of calling a provided function on every element in calling array.
       return(
-          <div>
-            <h3> Posts: </h3>
-            <ul>
-              {this.props.posts.map((b, i) => {
-                return(
-                  <tr key={i}>
-                    <li><Link to={`/posts/${b.id}`}>{b.title}</Link></li>
-                    <li>{ (isAuthenticated()) ? <Link to={`/editpost/${b.id}`}>Edit</Link> : '' } </li>
-                  </tr>
-                )
-              })}
-            </ul>
+          <div id="content" className="site-content">
+            <div className="container">
+              <div className="center-relative">
+                <h3> Posts: </h3>
+              </div>
+              <ul>
+                {this.props.posts.map((b, i) => {
+                  return(
+                    <tr key={i}>
+                      <article id="post-2" class="blog-item-holder">
+                        <div className="entry-content relative">
+                            <div className="content-1170 center-relative">
+                                <h2 className="entry-title">
+                                    <Link to={`/posts/${b.id}`}>{b.title}</Link>
+                                </h2>
+                                <div class="cat-links">
+                                    <ul>
+                                        <li>
+                                            <Link to="/">{b.category}</Link>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="entry-date published">{b.date}</div>
+                                <div className="excerpt">
+                                    INSERT EXCERPT HERE BRO.
+                                </div>
+                                <li>{ (isAuthenticated()) ? <Link to={`/editpost/${b.id}`}>Edit</Link> : '' } </li>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </article>
+                    </tr>
+                  )
+                })}
+              </ul>
+            </div>
           </div>
       )
     }
