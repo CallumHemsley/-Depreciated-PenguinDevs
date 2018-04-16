f84008e73fb42575693aaf0c46d7420e33440f30
CallumHemsley <bythepenguin@gmail.com>
2018-04-16 22:33:54 +0100

    added contact page.. too simple

---
+++ frontend/app/src/components/common/Contact.js
@@ -0,0 +1,48 @@
+// ./src/components/common/HomePage.js
+import React from 'react';
+import '../css/clear.css';
+import '../css/common.css';
+import '../css/sm-clean.css';
+import '../css/SideBar.css';
+import '../css/froala_blocks.css';
+import '../css/test.css';
+const Contact = () => {
+  return (
+    <div id="content" className="site-content center-relative">
+        <div className="single-post-wrapper content-1070 center-relative">
+            <article className="center-relative">
+                <div className="post-info content-660 center-relative">
+                    <div className="cat-links">
+                        <ul>
+                            <li>Email: cthemsley@gmail.com</li>
+                        </ul>
+                    </div>
+                    <div className="clear"></div>
+                    <div className="cat-links">
+                        <ul>
+                            <li>Github: CallumHemsley</li>
+                        </ul>
+                    </div>
+                    <div className="clear"></div>
+                    <div className="cat-links">
+                        <ul>
+                            <li>Twitter: @PenguinDevs</li>
+                        </ul>
+                    </div>
+                    <div className="clear"></div>
+                    <div className="cat-links">
+                        <ul>
+                            <li>Instagram: PenguinDevs</li>
+                        </ul>
+                    </div>
+                    <div className="clear"></div>
+                    <div className="clear"></div>
+                </div>
+                <div className="clear"></div>
+            </article>
+        </div>
+    </div>
+  );
+};
+
+export default Contact; 
