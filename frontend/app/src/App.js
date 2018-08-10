714d6235b5fa1b5027c6ec4c9e223ccbd3a519a6
CallumHemsley <bythepenguin@gmail.com>
2018-08-10 20:55:26 +0100

    google analytics

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactGA from 'react-ga';
 import { ConnectedRouter } from 'connected-react-router';
 import { Route } from 'react-router-dom';
 import Root from './components/Root';
+
+ReactGA.initialize('UA-123770507-1')
+
+function fireTracking() {
+  ReactGA.pageview(window.location.hash);
+}
 const App = ({ history }) => {
   return (
-    <ConnectedRouter history={history}>
+    <ConnectedRouter onUpdate={fireTracking} history={history}>
       <Route path="/" component={Root}/>
     </ConnectedRouter>
   )
